53525a1b3c63435f161264b5324511c4a012a1ce
bluestonecreeper <scottpickslay@gmail.com>
2025-11-26 00:51:57 -0800

    Implemented the scene

---
+++ src/main/java/cs112/ud3/FinalController.java
@@ -0,0 +1,79 @@
+package cs112.ud3;
+
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class FinalController {
+    Image blackBackground = new Image("black.png");
+
+    @FXML
+    private ImageView backgroundImage;
+    @FXML
+    private ImageView computerImage;
+
+    @FXML
+    private ImageView B1Image, B2Image, B3Image, B4Image, B5Image, B6Image;
+
+    @FXML
+    private Text pointInfo;
+
+    @FXML
+    protected void GetPoint() {
+        //increment points
+    }
+
+    @FXML
+    protected void U1() {
+        //increment points
+    }
+    @FXML
+    protected void U2() {
+        //increment points
+    }
+    @FXML
+    protected void U3() {
+        //increment points
+    }
+    @FXML
+    protected void U4() {
+        //increment points
+    }
+    @FXML
+    protected void U5() {
+        //increment points
+    }
+    @FXML
+    protected void U6() {
+        //increment points
+    }
+
+    @FXML
+    protected void B1() {
+        //increment points
+    }
+    @FXML
+    protected void B2() {
+        //increment points
+    }
+    @FXML
+    protected void B3() {
+        //increment points
+    }
+    @FXML
+    protected void B4() {
+        //increment points
+    }
+    @FXML
+    protected void B5() {
+        //increment points
+    }
+    @FXML
+    protected void B6() {
+        //increment points
+    }
+
+
+}
